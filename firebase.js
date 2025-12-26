679c1f8896f9ca5f8d89cfebd4eccaeefbb6dbe2
oseale <aleksi.piitulainen4@outlook.com>
2025-12-26 17:12:19 +0200

    Upgrade Firebase SDK and remove remote config
    Updated Firebase SDK imports to version 9.6.1 and removed remote config functionality.

--- firebase.js
+++ firebase.js
@@ -1,32 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
-import {
-  getRemoteConfig,
-  fetchAndActivate,
-  getValue
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-remote-config.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAHCYxUClxGazeEdwYeLPCM4bGDhjFCnCY",
   authDomain: "koulu-info-tv.firebaseapp.com",
   projectId: "koulu-info-tv",
-  storageBucket: "koulu-info-tv.firebasestorage.app",
-  messagingSenderId: "1002414133585",
   appId: "1:1002414133585:web:d4fabcf158c2bf60544370",
   measurementId: "G-PNEQEXEC7C"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-getAnalytics(app);
+const auth = getAuth(app);
 
-const remoteConfig = getRemoteConfig(app);
-remoteConfig.settings = {
-  minimumFetchIntervalMillis: 60_000  
-};
-remoteConfig.defaultConfig = {
-  refresh_version: "v1"
-};
-
-export { remoteConfig, fetchAndActivate, getValue };
-
+window.firebaseApp = app;
+window.firebaseAuth = auth;
