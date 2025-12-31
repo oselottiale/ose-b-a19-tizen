4a562d8d1042d8c01a4dca7f0cfa2d4c9b40a759
oseale <aleksi.piitulainen4@outlook.com>
2025-12-31 09:49:36 +0200

    Update index.loader.js

--- slides-edit-login/index.loader.js
+++ slides-edit-login/index.loader.js
@@ -2,5 +2,5 @@ firebaseAuth.currentUser.getIdToken().then(token => {
   const from = encodeURIComponent(window.location.href);
 
   window.location.href =
-    `https://script.google.com/macros/s/AKfycbzDeaRjRC7rAfxwpjGxI63igdamyDc8X2PBnbYVZmjDZ1DCmbM-HAumhUtxk_7ypDFQzA/exec?token=${token}&from=${from}`;
+    `https://script.google.com/macros/s/AKfycbyPDEf3_WietpXzUtwdAKbagY67WZGxzSpc6ZYYhS1W0vcliGE694CpckPXqTYlqcNuCQ/exec?token=${token}&from=${from}`;
 });
